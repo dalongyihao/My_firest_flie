9bc69d1e7ee31cf7c2429e3a679552cb3657f955
Dalongyihao <1245002131@qq.com>
2018-12-05 11:05:59 +0800

    awsfqawed

---
+++ 面向对象/面向对象案例.py
@@ -0,0 +1,15 @@
+import random
+def sjhs():
+    num = input("请输入一个1,2,3三个数字：")
+    rand_num =random.randrange(1,4)
+    num = int(num)
+    source = 0
+    if num > rand_num:
+        print("输入的数字比随机数大")
+    elif num == rand_num:
+        source +=100
+        print(source)
+    elif num < rand_num:
+         print("输入的数字比随机数小")
+    return sjhs()
+sjhs()
